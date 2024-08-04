fed716b8907ce6cc898c3b46410e6cfb689ad69d
Debi Prasanna Dash <130679362+debiprasannadash@users.noreply.github.com>
2024-08-04 22:06:03 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Reg from './Components/Register';
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="">
+        <Reg />
+      </div>
+    </main>
+  );
+}
