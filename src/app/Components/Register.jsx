0e9de7c170cf495025ef5c7372c1460be1544783
debiprasannadash <dp.dash12@gmail.com>
2024-08-11 17:39:37 +0530

    Changes committed

---
+++ src/app/Components/Register.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React from "react";
+
+function Register() {
+  return (
+    
+    <div className="App box-content h-auto w-auto bg-gradient-to-r from-violet-500 to-fuchsia-500">
+    
+        <div className="head flex flex-col justify-center items-center">
+          <img src="aws.svg" className="aws_logo bg-slate-700 my-4" alt="AWS Logo" />
+          <h1 className="text-center text-[55px] font-bold text-white">AWS KIIT Chapter Registration Page</h1>
+          </div>
+        
+        <br />
+        <form className="form_main box-content h-auto w-auto mx-[20%]">
+        <div className="reg ">
+          <p className=" text-[25px]">Required fields are marked by <b>*</b></p>
+          
+          <label>
+            <p className=" text-[20px]">Enter your Roll no. *</p>
+            <input type="text" required />
+          </label>
+          <br />
+          
+          <label>
+            <p className=" text-[20px]">Enter your name *</p>
+            <input type="text" required />
+          </label>
+          <br />
+          
+          <label>
+            <p className=" text-[20px]">Enter your mail id <b>(KIIT ID)</b> *</p>
+            <input type="email" required />
+          </label>
+          <br />
+
+          <p className=" text-[20px]">Select your academic year *</p>
+          <label>
+            <input type="radio" name="academicYear"  value="1" required /> 1
+          </label>
+          <label>
+            <input type="radio" name="academicYear" value="2" required /> 2
+          </label>
+          <label>
+            <input type="radio" name="academicYear" value="3" required /> 3
+          </label>
+          <br />
+
+          <p className=" text-[25px]">Gender *</p>
+          <label>
+            <input type="radio" name="gender" value="Male" required /> Male
+          </label>
+          <label>
+            <input type="radio" name="gender" value="Female" required /> Female
+          </label>
+          <br />
+
+          <label>
+            <p className=" text-[20px]">Date of Birth *</p>
+            <input type="date" required />
+          </label>
+          <br />
+
+          <label>
+            <p className=" text-[25px]">Enter your address *</p>
+            <textarea className="address" required></textarea>
+          </label>
+          <br />
+
+          <label>
+            <p className=" text-[25px]">Kindly upload your CV/Resume <b>(in PDF format)</b> *</p>
+            <input type="file" accept="application/pdf" required />
+          </label>
+          <br />
+          
+          <label>
+            <p className=" text-[25px]">Provide your LinkedIn URL *</p>
+            <input type="url" required />
+          </label>
+          <br />
+
+          <label>
+            <p className=" text-[25px]">Provide your GitHub Link *</p>
+            <input type="url" required />
+          </label>
+          <br />
+
+          <label>
+            <p className=" text-[25px]">What makes you feel a best fit for AWS KIIT Chapter? *</p>
+            <textarea required></textarea>
+          </label>
+          <br />
+
+          <button type="submit " className=" bg-pink-700 text-white font-bold px-[2%] py-[1%] rounded hover:bg-green-600">Register</button>
+          <br />
+          <div />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
